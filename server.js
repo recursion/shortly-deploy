c01dda44ae7e214eeed712ee3211c82dd0d46858
Michael Symmes <msymmes@gmail.com>
2015-06-10 19:26:25 -0400

    Console.log test message for server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var app = require('./server-config.js');
 
+console.log(process.env);
+
 // var port = 4568;
-var port = process.enc.PORT || 4568;
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
