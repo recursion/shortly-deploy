a37339c281d5cedc6527b2db918078efd18e0aab
Michael Symmes <msymmes@gmail.com>
2015-06-12 01:01:28 -0400

    Fixed DB config

--- app/config.js
+++ app/config.js
@@ -3,8 +3,5 @@ var mongoose = require('mongoose');
 // TODO: check for dev or production environment
 var db = mongoose.connect('mongodb://localhost/shortly');
 
-var host = process.env.DB || 'mongodb://localhost/shortly';
-var db = mongoose.connect(host);
-
 // export mongoose??
 module.exports = db;
